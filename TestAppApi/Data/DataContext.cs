c676a088a1a8ae07349c1a3581d61d24beab6cd4
Glen Roselle <glenroselle@gmail.com>
2019-04-23 19:36:45 -0400

    First dotnet committ

---
+++ TestAppApi/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using TestAppApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestAppApi.Data
+{
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+
+
+    }
+}
---
+++ TestAppApi/Models/Value.cs
@@ -0,0 +1,8 @@
+﻿namespace TestAppApi.Models
+{ 
+	public class Value
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
